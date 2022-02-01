2c9e05d7a480480225383076125bcc873cf0936f
Saexy <thekoala250303@gmail.com>
2022-02-01 15:50:54 -0300

    Remoção do componente NotFound

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import App from './App'
 import Cart from './components/Cart'
-import NotFound from './components/NotFound'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
@@ -14,7 +13,6 @@ ReactDOM.render(
     <Routes>
       <Route path="/" element={<App/>} exact/>
       <Route path="/cart" element={<Cart/>} exact/>
-      <Route path="*" element={<NotFound/>} exact/>
     </Routes>
   </Router>
   ,
